3b3057a2f97922d6092c1fa0ad9558ca37b975ce
Ismael Suarez <spawn@Ismaels-MacBook-Pro.local>
2020-07-26 22:50:43 -0500

    grocery_storeJS

---
+++ week1/grocery_storeJS/index.js
@@ -0,0 +1,26 @@
+// grocery list
+var groceryCart = ['milk', 'eggs', 'butter', 'cheese']
+
+var groceryItem = {
+    item: 'milk',
+    stockOnHand: 50,
+    isExpired: false
+}
+
+var groceryItem = {
+    item: 'eggs',
+    stockOnHand: 68,
+    isExpired: false
+}
+
+var groceryItem = {
+    item: 'butter',
+    stockOnHand: 34, 
+    isExpired: true
+}
+
+var groceryItem = {
+    item: 'cheese', 
+    stockOnHand: 54, 
+    isExpired: false
+}
