63d2c983112f97e19980a2724b7ff625183c174a
Ismael Suarez <spawn@Ismaels-MacBook-Pro.local>
2020-07-26 23:32:08 -0500

    grocery_storeJS

--- week1/grocery_storeJS/index.js
+++ week1/grocery_storeJS/index.js
@@ -13,17 +13,26 @@ var groceryItem = {
 var groceryItem = {
     item: 'eggs',
     stockOnHand: 68,
-    isExpired: false
+    isExpired: false,
+    stock: function () {
+        return this.item + '' + this.stockOnHand + '' + this.isExpired;
+    }
 }
 
 var groceryItem = {
     item: 'butter',
     stockOnHand: 34, 
-    isExpired: true
+    isExpired: true,
+    stock: function () {
+        return this.item + '' + this.stockOnHand + '' + this.isExpired;
+    }
 }
 
 var groceryItem = {
     item: 'cheese', 
     stockOnHand: 54, 
-    isExpired: false
+    isExpired: false,
+    stock: function () {
+        return this.item + '' + this.stockOnHand + '' + this.isExpired;
+    }
 }
