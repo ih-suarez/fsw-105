e7bb8a299a6391801279b0c9f8ccf6ba0d9d63d5
Ismael Suarez <spawn@Ismaels-MacBook-Pro.local>
2020-08-10 19:06:46 -0500

    dailyPlanet

--- week2/daily-planet-editor/daily-planet-editor/paper.js
+++ week2/daily-planet-editor/daily-planet-editor/paper.js
@@ -1,7 +1,6 @@
 /*
 This code contains only syntax and code style problems. The logic of the code works,
 so DO NOT change the functionality of anything in here.
-
 In short, you shouldn't need to add your own statements anywhere,
 just fix the existing ones.
 */
@@ -9,43 +8,41 @@ just fix the existing ones.
 
 // Written by Kent, Clark
 
-var enemies = ["Lex", "Batman", "Darkseid", "Brainiac", "General Zod", "Doomsday"];
+var enemies = ["Lex", "Batman", "Darkside", "Brainiac", "General Zod", "Doomsday"];
 
-function whoWins(is_there_kryptonite, enemies);{
-    if(is_there_kryptonite) {
-        return ("Superman beats " + enemies + ", of course");
+function whoWins(isThereKryptonite, enemyName){
+    if (isThereKryptonite){
+        return "Superman beats " +enemyName+ ", of course"
     } else {
-        return ("Depends on how quick Superman can get rid of the Kryptonite." + enemies + "could possibly win this one.");
-    }
-}
-
-for (var i=0;i<enemies.length;i++){
-        //var is_there_kryptonite
-            if (is_there_kryptonite % 2 === 0) {
-                return (is_there_kryptonite = true);
-            } else {
-                return (is_there_kryptonite = false);
+        return "Depends on how quick Superman can get rid of the Kryptonite." + enemyName + "could possibly win this one.";
         }
-    }     
-    console.log(Who_Wins(is_there_kryptonite,enemies[i]));
+    };
 
+for (var i = 0; i < enemies.length; i++){
+        var isThereKryptonite
+        if (i % 2 === 0) {
+            isThereKryptonite = true
+        } else {
+            isThereKryptonite = false
+        }
+        console.log(whoWins(isThereKryptonite, enemies[i]))
+};
 
-function isLoisAttracted(howAttractedIsLoisLaneToMe);{
+function isLoisAttractedToMe(){
     // 1 is not at all attracted, 10 is "super" attracted...
-        return (Math.floor((Mat.random()*10)+1));
-}
-
-consol.log(howAttractedIsLoisLaneToMe) ;
+    return Math.floor((Mat.random() * 10) + 1)
+};
+consol.log(isLoisAttractedToMe());
 
 var clarkKent = true;
 var superman = false;
+var phoneBoothQuickChange = Math.random();
 
-function when(clarkKent){
-    console.log("I'm just a nerdy columnist");
-    var phoneBoothQuickChange = Math.random();
+while (clarkKent){
+    console.log("I'm just a nerdy columnist")
     if (phoneBoothQuickChange >= 0.5) {
-        clarkKent = false;
-        superman = true;
-        console.log("Now I'm Superman!");
-    } 
-}
+        clarkKent = false
+        superman = true
+        console.log("Now I'm Superman!")
+    }
+};
